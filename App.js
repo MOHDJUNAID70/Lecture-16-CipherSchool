02ea321278d1da291adf6a1e4524560b1c1d853a
MOHD JUNAID <119116032+MOHDJUNAID70@users.noreply.github.com>
2024-07-11 12:33:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+const nam="Mohd Junaid"
+
+  return (
+    <div>
+    <div>
+      <h1 style={{
+        color: "red",
+        textAlign: "center",
+      }}>Hello from the {nam}!</h1>
+    </div>
+      <h2 style={{
+        color: "cyan",
+        textAlign: "center",
+      }}>hello everyone!</h2>
+    </div>
+  )
+}
+
+export default App;
